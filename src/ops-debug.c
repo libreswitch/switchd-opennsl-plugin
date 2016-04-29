b0f182d3047cd22ad87086a85049135f3dbd6a52
midhan <nitish.midha@hpe.com>
2016-04-29 11:42:58 -0700

    chg: dev: Diag-dump for lag.
    TG-1073:
    
    Change-Id: I65f6d7be1cd3b2e207fedcd4f73c61da1919754b
    Signed-off-by: midhan <nitish.midha@hpe.com>
    (cherry picked from commit b14c69fad2fc668fe073d760873c04d732349aae)

--- src/ops-debug.c
+++ src/ops-debug.c
@@ -1702,6 +1702,22 @@ static void copp_diag_dump_cb(char *buf)
     snprintf(buf, ds.length, "%s", ds_cstr(&ds));
 }
 
+static void
+lag_diag_dump_basic_cb(char *buf)
+{
+    struct ds ds = DS_EMPTY_INITIALIZER;
+
+    /* populate basic diagnostic data to buffer  */
+    ds_put_format(&ds, "LAG information:\n");
+    ops_lag_dump(&ds, -1);
+    ds_put_format(&ds, "\n\n");
+    snprintf(buf, ds.length, "%s", ds_cstr(&ds));
+
+    diag_dump_basic_cb(buf+ds.length-1);
+
+}
+
+
 /* _diag_dump_callback */
 /**
  * callback handler function for diagnostic dump basic
@@ -1718,7 +1734,6 @@ static void diag_dump_callback(const char *feature , char **buf)
     *buf =  xcalloc(1,DIAGNOSTIC_BUFFER_LEN);
     if (*buf) {
         if (!strncmp(feature, L3INTERFACE, strlen(L3INTERFACE)) ||
-            !strncmp(feature, LAGINTERFACE, strlen(LAGINTERFACE)) ||
             !strncmp(feature, SUBINTERFACE, strlen(SUBINTERFACE)) ||
             !strncmp(feature, LOOPBACK, strlen(LOOPBACK)) ||
             !strncmp(feature, VLANINTERFACE, strlen(VLANINTERFACE))) {
@@ -1727,6 +1742,8 @@ static void diag_dump_callback(const char *feature , char **buf)
             copp_diag_dump_cb(*buf);
         } else if (!strncmp(feature, SFLOW, strlen(SFLOW))) {
             sflow_diag_dump_basic_cb(*buf);
+        } else if (!strncmp(feature, LAGINTERFACE, strlen(LAGINTERFACE))) {
+            lag_diag_dump_basic_cb(*buf);
         }
     } else {
         VLOG_ERR("Memory allocation failed for feature %s , %d bytes",
