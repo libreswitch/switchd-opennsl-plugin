OpenSwitchNOS/openswitch-ops-switchd-opennsl-plugin

Strip internal VLAN tags from sampled packets
Tags: fix,dev
TG-1225
* Changes to keep track of internal VLANs and strip the VLAN
  tag from the sampled packet before sending to sFlow collector

Change-Id: I2900a4e70df906e088a9c79cc20a7cfdf57b7ea7
Signed-off-by: Vasanth Viswanathan <vasanth.viswanathan@hpe.com>

include/ops-routing.h
@@ -182,5 +182,6 @@ extern opennsl_field_group_t ops_routing_get_ospf_group_id_by_hw_unit (
                                                                 int unit);
 extern void ops_l3_mac_move_add(int unit, opennsl_l2_addr_t *l2addr, void *userdata);
 extern void ops_l3_mac_move_delete(int unit, opennsl_l2_addr_t *l2addr, void *userdata);
+extern bool ops_routing_check_is_internal_vlan(opennsl_vlan_t vlan);
 
 #endif /* __OPS_ROUTING_H__ */

libreswitch/switchd-opennsl-plugin

include/ops-routing.h
@@ -182,5 +182,6 @@ extern opennsl_field_group_t ops_routing_get_ospf_group_id_by_hw_unit (
                                                                 int unit);
 extern void ops_l3_mac_move_add(int unit, opennsl_l2_addr_t *l2addr, void *userdata);
 extern void ops_l3_mac_move_delete(int unit, opennsl_l2_addr_t *l2addr, void *userdata);
+extern bool ops_routing_is_internal_vlan(opennsl_vlan_t vlan);
 
 #endif /* __OPS_ROUTING_H__ */